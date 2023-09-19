e452d2b26e807916fc455ebf64c18f590201df72
jeang-bo-yuan <jeangbyj@gmail.com>
2023-09-19 10:52:10 +0800

    微調+新增Filter::Filter_t::at

--- Project1/src/Filter.h
+++ Project1/src/Filter.h
@@ -17,21 +17,21 @@ namespace Filter {
 	};
 
 	//! a Size * Size filter
-	template <size_t Size>
+	template <int Size>
 	class Filter_t {
-		static_assert((Size & 1), "Size must be odd");
+		static_assert((Size & 1) && Size > 0, "Size must be odd and positive");
 
 		float mask[Size][Size]; //!< Size列 Size欄
 
 	public:
 		//! @brief construct from a list, ex: { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }
-		//! @param _list - a list contains lists of float. It's undefined, if `_list`'s size is bigger than Size * Size
+		//! @param _list - a list contains lists of float. std::out_of_range is thrown, if `_list`'s size is bigger than Size * Size
 		Filter_t(std::initializer_list<std::initializer_list<float>> _list) : mask{0} {
 			int r = 0;
 			for (const auto& row : _list) {
 				int c = 0;
 				for (const auto& elem : row) {
-					mask[r][c] = elem;
+					this->at(r, c) = elem;
 					++c;
 				}
 				++r;
@@ -43,7 +43,7 @@ namespace Filter {
 		//! @param c - column of pixel
 		//! @param image - image's info
 		Color::RGB_t calculate(int r, int c, const ImageInfo_t& image) const {
-			int half_size = Size / 2;
+			constexpr int half_size = Size / 2;
 			float result[3] = { 0, 0, 0 };
 
 			// return true if (r, c) is outside image 
@@ -57,15 +57,29 @@ namespace Filter {
 
 					int id = ((r + deltaR) * image.width + (c + deltaC)) * 4;
 					// 乘上的係數
-					float multiplier = mask[deltaR + half_size][deltaC + half_size];
+					float rate = mask[deltaR + half_size][deltaC + half_size];
 
-					result[0] += image.data[id] * multiplier;
-					result[1] += image.data[id + 1] * multiplier;
-					result[2] += image.data[id + 2] * multiplier;
+					result[0] += image.data[id] * rate;
+					result[1] += image.data[id + 1] * rate;
+					result[2] += image.data[id + 2] * rate;
 				}
 			}
 
 			return Color::RGB_t((uint8_t)result[0], (uint8_t)result[1], (uint8_t)result[2]);
 		}
-	};
+
+		/**
+		 * @brief 存取mask中的元素，若超出範圍則丟出std::out_of_range
+		 * @param r - 列
+		 * @param c - 欄
+		 */
+		inline float& at(int r, int c) {
+			if (r >= Size || c >= Size || r < 0 || c < 0) throw std::out_of_range("Filtert_t::at() : Out of range");
+			return mask[r][c];
+		}
+		inline const float& at(int r, int c) const {
+			if (r >= Size || c >= Size || r < 0 || c < 0) throw std::out_of_range("Filtert_t::at() : Out of range");
+			return mask[r][c];
+		}
+	}; // class Filter_t
 }
