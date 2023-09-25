c245dcca4830da591075d75c50dbc9a8c948a2bb
jeang-bo-yuan <jeangbyj@gmail.com>
2023-09-25 21:04:17 +0800

    Add glm library + Fix the bug of My::perspective

--- Project2/src/My_GLU.cpp
+++ Project2/src/My_GLU.cpp
@@ -1,30 +1,11 @@
 #include "My_GLU.h"
 #include <cmath>
-
-namespace {
-	void normalize_d(GLdouble* vec, GLint N) {
-		// calculate the vector's length
-		GLdouble length = 0;
-		for (GLint i = 0; i < N; ++i) {
-			length += (vec[i] * vec[i]);
-		}
-		length = sqrt(length);
-
-		for (GLint i = 0; i < N; ++i) {
-			vec[i] /= length;
-		}
-	}
-
-	void cross_product_d(GLdouble* out, const GLdouble* u, const GLdouble* v) {
-		out[0] = u[1] * v[2] - v[1] * u[2];
-		out[1] = -u[0] * v[2] + v[0] * u[2];
-		out[2] = u[0] * v[1] - v[0] * u[1];
-	}
-}
+#include <glm/vec3.hpp>
+#include <glm/geometric.hpp>
 
 void My::perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar)
 {
-	GLdouble f = 1.0 / tan((fovy * 180 / 3.14) / 2.0);
+	GLdouble f = 1.0 / tan((fovy * 3.14 / 180) / 2.0);
 	// store 4*4 matrix in column-major
 	GLdouble m[16] = {
 		f / aspect, 0, 0, 0,
@@ -39,30 +20,24 @@ void My::lookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
 	GLdouble centerX, GLdouble centerY, GLdouble centerZ,
 	GLdouble upX, GLdouble upY, GLdouble upZ)
 {
-	GLdouble F[3] = { centerX - eyeX, centerY - eyeY, centerZ - eyeZ };
-	normalize_d(F, 3);
-	GLdouble UP[3] = { upX, upY, upZ };
-	normalize_d(UP, 3);
+	glm::dvec3 F(centerX - eyeX, centerY - eyeY, centerZ - eyeZ);
+	F = glm::normalize(F);
 
-	GLdouble M[4][4] = { {0} };
-	M[0][2] = -F[0];
-	M[1][2] = -F[1];
-	M[2][2] = -F[2];
-	M[3][3] = 1;
+	glm::dvec3 UP(upX, upY, upZ);
+	UP = glm::normalize(UP);
 
-	GLdouble s[3];
-	cross_product_d(s, F, UP);
-	M[0][0] = s[0];
-	M[1][0] = s[1];
-	M[2][0] = s[2];
+	glm::dvec3 s = glm::cross(F, UP);
 
-	GLdouble u[3];
-	normalize_d(s, 3);
-	cross_product_d(u, s, F);
-	M[0][1] = u[0];
-	M[1][1] = u[1];
-	M[2][1] = u[2];
+	glm::dvec3 u = glm::cross(glm::normalize(s), F);
 
-	glMultMatrixd(&M[0][0]);
+	// column-major
+	double M[16] = {
+		s[0], u[0], -F[0], 0,
+		s[1], u[1], -F[1], 0,
+		s[2], u[2], -F[2], 0,
+		0   , 0   , 0    , 1
+	};
+
+	glMultMatrixd(M);
 	glTranslated(-eyeX, -eyeY, -eyeZ);
 }
