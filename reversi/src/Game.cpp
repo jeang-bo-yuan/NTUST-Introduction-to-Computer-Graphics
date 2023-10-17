58be9931ec8efe48a1d0ee7388f924830b4ab81a
jeang-bo-yuan <jeangbyj@gmail.com>
2023-10-17 19:43:10 +0800

    Game method prototype

--- reversi/src/Game.cpp
+++ reversi/src/Game.cpp
@@ -1,8 +1,34 @@
 
 #include "Game.h"
+#include <string.h>
 
 Game::Game()
 {
+    this->reset();
+}
 
+bool Game::click(size_t row, size_t col)
+{
+    if (m_is_dark_turn) {
+        m_board[row][col] = Disk::Dark;
+    }
+    else {
+        m_board[row][col] = Disk::Light;
+    }
+
+    m_is_dark_turn = !m_is_dark_turn;
+
+    return true;
+}
+
+void Game::reset()
+{
+    memset(m_board, 0, sizeof(m_board));
+    m_board[3][3] = Disk::Light;
+    m_board[4][4] = Disk::Light;
+    m_board[3][4] = Disk::Dark;
+    m_board[4][3] = Disk::Dark;
+
+    m_is_dark_turn = true;
 }
 
--- reversi/src/Game.h
+++ reversi/src/Game.h
@@ -1,33 +1,106 @@
 /**
  * @file Game.h
- * @brief 儲存遊戲的class
+ * @brief 代表遊戲的class
  */
 #ifndef GAME_H
 #define GAME_H
 
 #include <string>
 
-
+/// 代表遊戲的class
 class Game
 {
 public:
-    enum class Disk {
-        None = 0,   //!< 沒有棋子
-        Dark = 1,   //!< 黑色的棋子（先手）
-        Light = 2,  //!< 白色的棋子（後手）
+    /// 棋子的種類
+    enum class Disk : char {
+        None = 0,     //!< 沒有棋子
+        Dark = 'D',   //!< 黑色的棋子（先手）
+        Light = 'L',  //!< 白色的棋子（後手）
+    };
+
+    /// 代表遊玩狀態
+    enum class State : char {
+        Playing = 'P',   //!< 遊戲進行中
+        Dark_Win = 'W',  //!< 黑色贏了
+        Light_Win = 'w', //!< 白色贏了
+        Draw = 'D'       //!< 平手
     };
 
 private:
+    /// 棋盤
     Disk m_board[8][8];
 
+    /// 是否為黑色的回合
+    bool m_is_dark_turn;
+
+    /// 遊玩狀態
+    State m_state;
+
 public:
+    /**
+     * @brief constructor，建構後立即呼叫Game::reset
+     */
     Game();
 
-    void restart();
+    /// @name Access The Board
+    /// Note: row和col都要在`[0, 8)`間，函數不會做邊界檢查
+    /// @{
 
-    void writeToFile(const std::string& file_name);
+    /**
+     * @brief 取得盤面上的棋子
+     * @param row - 列
+     * @param col - 欄
+     * @return 棋子的種類 Game::Disk
+     */
+    Disk get_disk(size_t row, size_t col) const {
+        return m_board[row][col];
+    }
 
-    void readFromFile(const std::string& file_name);
+    /**
+     * @brief 點擊，若成功則放上棋子並進入下一回合
+     * @param row - 列
+     * @param col - 欄
+     * @return 放置成功則回傳`true`，否則回傳`false`
+     * @todo
+     * 1. 確認能不能放
+     * 2. 進入下一回合時，要計算能走的棋步
+     */
+    bool click(size_t row, size_t col);
+
+    /// @}
+
+    /// @name Game State Management
+    /// @{
+
+    /**
+     * @brief 取得遊戲的狀態（state）
+     * @return Game::State 中的其中一個
+     */
+    State get_state() const {
+        return m_state;
+    }
+
+    /**
+     * @brief 確認是不是黑色的回合
+     * @return 如果是黑色的回合，回傳`true`；否則回傳`false`
+     */
+    bool is_dark_turn() const {
+        return m_is_dark_turn;
+    }
+
+    /**
+     * @brief 重設棋盤和遊戲狀態（即重新開始）
+     * @details 設成黑色的回合，狀態（state）設成 State::Playing ，且棋盤只有中間四格有棋
+     * ```
+     * --- ---- ---
+     * --- 白 黑 ---
+     * --- 黑 白 ---
+     * --- ---- ---
+     * ```
+     */
+    void reset();
+
+    /// @}
 };
 
 #endif // GAME_H
