53a10c243edfb34b7ecb6956393f5683e4f7b950
jeang-bo-yuan <jeangbyj@gmail.com>
2023-10-20 18:09:13 +0800

    改UI

--- reversi/src/MainWindow.cpp
+++ reversi/src/MainWindow.cpp
@@ -9,7 +9,8 @@ MainWindow::MainWindow(QWidget *parent) :
     this->update_info();
 
     connect(ui->view, &GameView::clicked_success, this, &MainWindow::update_info);
-    connect(ui->menuRestart, &QMenu::aboutToShow, this, &MainWindow::restart);
+    ui->menuFrame->layout()->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    connect(ui->buttonRestart, &QPushButton::clicked, this, &MainWindow::restart);
 }
 
 MainWindow::~MainWindow()
@@ -19,12 +20,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::update_info()
 {
-    if (m_game_ptr->is_dark_turn()) {
-        ui->labelTurn->setText("Dark's\nTurn");
-    }
-    else {
-        ui->labelTurn->setText("Light's\nTurn");
-    }
+    ui->labelDarkTurn->hide();
+    ui->labelLightTurn->hide();
+    ui->labelDarkWin->hide();
+    ui->labelLightWin->hide();
+    ui->labelDraw->hide();
 
     ui->lcdDark->display(m_game_ptr->get_dark_num());
     ui->lcdLight->display(m_game_ptr->get_light_num());
@@ -32,15 +32,24 @@ void MainWindow::update_info()
     switch (m_game_ptr->get_state()) {
     case Game::State::Playing:
         ui->statusbar->showMessage("Playing");
+        if (m_game_ptr->is_dark_turn()) {
+            ui->labelDarkTurn->show();
+        }
+        else {
+            ui->labelLightTurn->show();
+        }
         break;
     case Game::State::Dark_Win:
         ui->statusbar->showMessage("Dark Wins");
+        ui->labelDarkWin->show();
         break;
     case Game::State::Light_Win:
         ui->statusbar->showMessage("Light Wins");
+        ui->labelLightWin->show();
         break;
     case Game::State::Draw:
         ui->statusbar->showMessage("Draw");
+        ui->labelDraw->show();
         break;
     }
 }
